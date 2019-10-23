fe1ec3be1436b34252d95a827261c40b7b4e957b
mysterymagination <creswel2@gmail.com>
2019-10-22 21:15:12 -0400

    init game template

---
+++ mootywortadv.game.en.js
@@ -0,0 +1,78 @@
+/**
+ * IFID
+ * 
+ * UUID://AD73E1D4-B6EF-4876-9099-BF0EDAF0B842//
+ */
+undum.game.id = "AD73E1D4-B6EF-4876-9099-BF0EDAF0B842";
+
+/* A string indicating what version of the game this is. Versions are
+ * used to control saved-games. If you change the content of a game,
+ * the saved games are unlikely to work. Changing this version number
+ * prevents Undum from trying to load the saved-game and crashing. */
+undum.game.version = "1.0";
+
+/* A variable that changes the fade out speed of the option text on
+ * a mobile. */
+undum.game.mobileHide = 2000
+
+/* A variable that changes the options fade out speed. */
+undum.game.fadeSpeed = 1500
+
+/* A variable that changes the slide up speed after clicking on an
+ * option. */
+undum.game.slideUpSpeed = 500
+
+undum.game.situations = {
+ // todo: stub
+}
+
+/* The Id of the starting situation. */
+undum.game.start = "main";
+
+/* Here we define all the qualities that our characters could
+ * possess. We don't have to be exhaustive, but if we miss one out then
+ * that quality will never show up in the character bar in the UI. */
+undum.game.qualities = {
+    health: new undum.NumericQuality(
+        "Health", {priority:"0001", group:'stats'}
+    ),
+    stamina: new undum.NumericQuality(
+        "Stamina", {priority:"0002", group:'stats'}
+    ),
+    moleWhole: new BurrowAdjectivesQuality( 
+        "<span title='One\'s ability to dig is not measured in kilograms of displaced dirt -- rather, it must take into account all the courage, curiosity, and tenacity required of tunnelers of all stripes.  What can your claws do?  Where can they take you?  We shall see!'>Mole Whole</span>",
+        {priority:"0003", group:'stats'}
+    )
+};
+
+/* A specialization of WordScaleQuality that uses the standard mole burrowing ordinal 
+    * adjective scale (from 'surfacer' at -3 to 'delver' at +3). 
+    */
+var BurrowAdjectivesQuality = function(title, opts) {
+    WordScaleQuality.call(this, title, [
+        "surfacer".l(), "noodler".l(), "wornclawed".l(),
+        "porpoise of the earth".l(), "tunnelfish".l(), "digger".l(), "delver".l()
+    ], opts);
+    if (!('offset' in opts)) this.offset = -3;
+};
+BurrowAdjectivesQuality.inherits(WordScaleQuality);
+
+// ---------------------------------------------------------------------------
+/* The qualities are displayed in groups in the character bar. This
+ * determines the groups, their heading (which can be null for no
+ * heading) and ordering. QualityDefinitions without a group appear at
+ * the end. It is an error to have a quality definition belong to a
+ * non-existent group. */
+undum.game.qualityGroups = {
+    stats: new undum.QualityGroup(null, {priority:"0001"})
+};
+
+// ---------------------------------------------------------------------------
+/* This function gets run before the game begins. It is normally used
+ * to configure the character at the start of play. */
+undum.game.init = function(character, system) {
+    character.qualities.health = 10;
+    character.qualities.stamina = 10;
+    character.qualities.aptitude = 0;
+    system.setCharacterText("<p>You are starting on an exciting journey beneath the earth and beyond all reason.</p>");
+};
