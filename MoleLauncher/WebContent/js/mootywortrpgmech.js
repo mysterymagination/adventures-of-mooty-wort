222c1feb2cad1b8bdf107272138e841a77e7c3e6
mysterymagination <creswel2@gmail.com>
2020-03-20 20:02:02 -0400

    fleshing out mole rpgmech module

--- MoleLauncher/WebContent/js/mootywortrpgmech.js
+++ MoleLauncher/WebContent/js/mootywortrpgmech.js
@@ -234,6 +234,280 @@ function MootyWortRpgMech() {
     mootyChar.stats["res"] = 10;
     mootyChar.entity = new window.Entity({ name: "Burrower" });
 
+    var grueChar = this.characters["grue"];
+    grueChar.gender = "male";
+    grueChar.stats["maxHP"] = 250;
+    grueChar.stats["maxMP"] = 500;
+    grueChar.stats["hp"] = grueChar.stats["maxHP"];
+    grueChar.stats["mp"] = grueChar.stats["maxMP"];
+    grueChar.stats["atk"] = 75;
+    grueChar.stats["def"] = 100;
+    grueChar.stats["pwr"] = 100;
+    grueChar.stats["res"] = 25;
+    grueChar.entity = new this.Entity({ name: "Heart of Darkness" });
+    
+    /**
+    Touch of the Void deals moderate HP and MP damage if the target has any MP remaining, and the MP is absorbed by Grue. Else, it deals heavy HP damage.  Only usable once every 3-5 turns, and there's a tell in the log when it recharges.
+    */
+    var touchVoidSpell = new lib.Spell({ id: "touch_of_the_void", name: "Touch of the Void" });
+    touchVoidSpell.targetType = lib.Ability.TargetTypesEnum.singleEnemy;
+    touchVoidSpell.cost = { "mp": 0, "hp": 15 };
+    /**
+     * Calculate the damage to HP or MP to the given target character
+     * @param sourceChar the source of the spell
+     * @param targetChar the target of the spell
+     * @param isMagic true if we're calculating the magical aspect of this spell's damage, false otherwise
+     */
+    touchVoidSpell.calcDmg = function (sourceChar, targetChar, isMagic) {
+    	let aspectDamage = 0.0;
+    	if(isMagic) {
+    		aspectDamage = sourceChar.attributes["pwr"] - 0.5 * targetChar.stats["res"];
+    	} else {
+    		aspectDamage = sourceChar.stats["atk"] - 0.5 * targetChar.stats["def"];
+    	}
+    	return aspectDamage;
+    }
+    touchVoidSpell.effect = function (sourceChar, targetChar) {
+        if(targetChar.stats["mp"] > 0) {
+        	// target has MP to steal so apply damage to HP and MP, and source's MP is healed
+        	this.dmg = this.calcDmg(sourceChar, targetChar, true);
+            targetChar.stats["mp"] -= this.dmg;
+            sourceChar.stats["mp"] += this.dmg;
+            this.dmg = this.calcDmg(sourceChar, targetChar, false);
+            targetChar.stats["hp"] -= this.dmg;
+        } else {
+        	// apply double damage to target HP, no one is healed
+        	this.dmg = this.calcDmg(sourceChar, targetChar, false);
+            targetChar.stats["hp"] -= 2 * this.dmg;
+        }
+
+        // MP cost
+        this.processCost(sourceChar);
+    }
+    touchVoidSpell.generateFlavorText = function (sourceChar, targetChar) {
+        return "Somehow the darkness near your rump becomes material and seizes you!  Cold infects your being as all the warmth and joy of life bleeds away, slaking the implacable thirst of Darkness.";
+    
+    /**
+     * Insatiable Consumption is an instant kill if the mole is below 50% HP and otherwise drops him to 50% HP
+     */
+    var consumeSpell = new lib.Spell({ id: "consume", name: "Insatiable Consumption" });
+    consumeSpell.targetType = lib.Ability.TargetTypesEnum.singleEnemy;
+    consumeSpell.cost = { "mp": 25 };
+    consumeSpell.effect = function (sourceChar, targetChar) {
+        let currentHP = targetChar.stats["hp"];
+        let maxHP = targetChar.stats["maxHP"]
+    	if(currentHP < 0.5 * maxHP) {
+    		targetChar.stats["hp"] = 0;
+    	} else {
+    		targetChar.stats["hp"] = maxHP * 0.5;
+    	}
+
+        // MP cost
+        this.processCost(sourceChar);
+    }
+    consumeSpell.generateFlavorText = function (sourceChar, targetChar) {
+        return "A thunderclap of malevolent intent momentarily deafens you; in the midst of that silence, when the wave of emptiness in your surroundings seems to reach its peak, a lightning flash of fangs manifests out of nothing in a rictus grin.  Jaws spreading wide, it chomps down and attempts to swallow you whole!  "+(targetChar["hp"] <= 0 ? "Your bloodsoaked fur helps ease your way on in and down, and in an instant the champion of Deepness is consumed." : "Your bulk and mighty constitution prevent you from sliding down the creature's gullet, but the fangs are still able to tear into your flesh."); 
+    }
+    
+    /**
+     * Brass Lantern uses the target's raw magic power to deal damage to it, ignoring defense.  
+     * It then doubles target's magic power such that subsequent hits will be worse.   
+     */
+    var brassLanternSpell = new lib.Spell({ id: "brass_lantern", name: "Brass Lantern" });
+    brassLanternSpell.targetType = lib.Ability.TargetTypesEnum.singleEnemy;
+    brassLanternSpell.cost = { "mp": 10 };
+    brassLanternSpell.effect = function (sourceChar, targetChar) {
+    	
+    	// deal damage equal to current mag pwr
+        targetChar["hp"] -= targetChar["pwr"];
+        // increase mag pwr as the mystic inferno infuses target's soul
+        targetChar["pwr"] *= 2;
+        	
+        // MP cost
+        this.processCost(sourceChar);
+    }
+    brassLanternSpell.generateFlavorText = function (sourceChar, targetChar) {
+        return "A fierce gold light burns its way out of the darkness, revealing a small brass lantern.  Inside, a flame flickers violently, tauntingly, before flaring into a raging inferno that rolls over you like a blanket of elemental destruction!  You can feel thoughts and emotions swirl in your mind as you burn, dreams flitting past your mind's eye and feeding the conflagration.  "+(targetChar["hp"] > 0 ? "As the flames roll over you, through the crippling agony you feel a resonant power welling up ever higher..." : ""); 
+    }
+    // todo: placeholder define dark_star (high MP cost for big damage)
+    /**
+    Dark Star deals massive non-elemental damage to all enemies
+    */
+    var darkStarSpell = new lib.Spell({ id: "dark_star", name: "Dark Star" });
+    darkStarSpell.targetType = lib.Ability.TargetTypesEnum.allEnemies;
+    darkStarSpell.cost = { "mp": 25 };
+    darkStarSpell.calcDmg = function (sourceChar, targetChar) {
+    	return 2 * sourceChar.stats["pwr"] 
+        	   - 0.5 * targetChar.stats["res"];
+    }
+    darkStarSpell.effect = function (sourceChar, targetChars) {
+    	for(let index = 0; index < targetChars.length; index++) {
+        	// apply damage to target
+        	this.dmg = this.calcDmg(sourceChar, targetChars[index]);
+        	targetChars[index].stats["hp"] -= this.dmg;
+        }
+
+        // MP cost
+        this.processCost(sourceChar);
+    }
+    darkStarSpell.generateFlavorText = function (sourceChar, targetChar) {
+        return "The burning chill of moonless midnight wrapped in Lady Winter's empty embrace casts a pall of hoarfrost over your fur as the light drains out of the world.  When all is naught but silence and dark, a muted gray pinprick of light appears before you; an offering of hope.  Unable to help yourself, you reach out to it -- the very instant you give over the focus of your mind to its power, it explodes into a blinding nova whose insatiable devouring flames crawl into and over every atom of your being!"; 
+    }
+
+    grueChar.runAI = function (combat, role) {
+        console.log("reached grue runAI fn... have fn!");
+        if (role) {
+            if (role === "enemy") {
+                var chosenAbility = undefined;
+                var chosenTarget = this.characters["mole"];
+
+                var playerParty = [];
+                for (let playerCharacter of combat.playerParty) {
+                    playerParty.push(this.characters[playerCharacter.id]);
+                    console.log("attempting to add character with id " + playerCharacter.id);
+                    console.log("added " + this.characters[playerCharacter.id].name + " to playerParty");
+                }
+            
+                var enemyParty = [];
+                for (let enemyCharacter of combat.enemyParty) {
+                    enemyParty.push(this.characters[enemyCharacter.id]);
+                }
+
+                // outliers and statistical points of interest
+                var playerLeastDefense = this.characters["mole"];
+                var playerLeastHP = this.characters["mole"];
+                var playerWithTargetBuff = undefined;
+                var anyPlayerOffenseBuffed = false;
+                var maxHealth = true; // assume true and let contradiction flip it
+
+                /// begin gathering data ///
+                for (let player of playerParty) {
+
+                    // overwrite player with least defense if applicable
+                    if (player.stats["def"] < playerLeastDefense.stats["def"]) {
+                        playerLeastDefense = player;
+                    }
+
+                    // overwrite player with least HP if applicable
+                    if (player.stats.hp < playerLeastHP.stats.hp) {
+                        playerLeastHP = player;
+                    }
+
+                    // check for max health scenario flip
+                    if (player.stats.hp < player.stats.maxHP) {
+                        maxHealth = false;
+                    }
+
+                }// end for each player
+                /// end gathering data + maybe buff balancing ///
+
+                /// begin story scenario modifier processing ///
+                // todo: check mole type and maybe modify attack choice accordingly
+                /// end story scenario mod proc ///
+
+                // begin defaults block -- at this point in the script, 
+                // if The God has not already picked an abl he will either 
+                // do one mostly at random OR spam Dark Star if his health is below 25%
+                if (chosenAbility === undefined && chosenTarget === undefined) {
+                    if (this.stats.hp > this.stats.maxHP * 0.25) {
+                        var percentageRandoAbl = lib.rollPercentage();
+                        // choose a player party index randomly and pull the poor person from it for targeting
+                        var playerRandoTarget = playerParty[Math.floor(Math.random() * playerParty.length)];
+                        // all The God's special abilities are pretty punishing, so make basic atk most probably
+                        if (percentageRandoAbl <= 35) {
+                        	// basic attack
+                            chosenAbility = this.abilities["attack"];
+                            chosenTarget = playerLeastDefense;
+                        } else if (percentageRandoAbl > 36 && percentageRandoAbl <= 60) {
+                        	// hug if possible for maximum damage output
+                        	if(this.canAffordCost(this.spells["manyfold_embrace"])) {
+                        		chosenAbility = this.spells["manyfold_embrace"];
+                        	} else {
+                        		chosenAbility = this.abilities["attack"];
+                        	}
+                            chosenTarget = playerRandoTarget;
+                        } else if (percentageRandoAbl > 61 && percentageRandoAbl <= 85) {
+                        	// either apply bloodlust to increase attack, or attack to benefit from it
+                        	if (!lib.hasStatusEffect(this, lib.statusEffectsDict["bloodlust"])) {
+                        		chosenAbility = this.spells["primordial_mandate"];
+                                chosenTarget = this;
+                            } else {
+                            	// hug if possible for maximum damage output
+                            	if(this.canAffordCost(this.spells["manyfold_embrace"])) {
+                            		chosenAbility = this.spells["manyfold_embrace"];
+                            	} else {
+                            		chosenAbility = this.abilities["attack"];
+                            	}
+                            	// regardless, hit the weakest opponent
+                                chosenTarget = playerLeastDefense;
+                            }
+                        } else {
+                        	chosenAbility = this.spells["putrefaction"];
+                            chosenTarget = playerParty;
+                        }// end rando block
+                    }// end The God HP > 25%
+                    else {
+                        // being severely injured, The God now starts to spam Dark Star if no earlier behaviors were proced and he can afford it
+                    	if(theGodChar.canAffordCost(darkStarSpell)) {
+                    		chosenAbility = this.spells["dark_star"];
+                    		chosenTarget = playerParty;
+                    	} else {
+                    		chosenAbility = this.abilities["attack"];
+                    		chosenTarget = playerLeastDefense;
+                    	}
+                    }
+                } // end if abl and target are not yet chosen, landing us in defaults
+                /// end defaults block ///
+
+                // normalize input chosen targets to array form
+                var targets = undefined;
+                if (chosenAbility.targetType === lib.Ability.TargetTypesEnum.allEnemies) {
+                    console.log("setting AI targets to array " + chosenTarget + " which starts with " + chosenTarget[0].name);
+                    targets = chosenTarget;
+                } else {
+                    // in this case there is only one target, but by wrapping it in an array we can proceed with equivalent loop code used for multi-target scenario
+                    console.log("setting AI target to array wrapping the single target " + chosenTarget + " with name " + chosenTarget.name);
+                    targets = [chosenTarget];
+                }
+
+                console.log("AI chose the ability " + chosenAbility.name);
+                if (targets.length > 0) {
+                    // there are still targets, so go forward with them as per usual
+                    if (chosenAbility.targetType === lib.Ability.TargetTypesEnum.allEnemies) {
+
+                        // multi-target attack, expects an array of chars as target	
+                        chosenAbility.effect(this, targets);
+                        combat.combatLogContent = chosenAbility.generateFlavorText(this, targets);
+
+                        for (let targetKey in targets[0]) {
+                            console.log("target " + targets[0] + " with name " + targets[0].name + " has prop: " + targetKey);
+                        }
+
+                        console.log(targets[0].name + "'s hp is now " + targets[0].stats.hp + " and specifically the human's HP is " + this.characters["mole"].stats.hp);
+
+                    } else {
+                        // single target attack, expects only single character as target
+                        chosenAbility.effect(this, targets[0]);
+                        combat.combatLogContent = chosenAbility.generateFlavorText(this, targets[0]);
+
+                        for (let targetKey in targets[0]) {
+                            console.log("target " + targets[0] + " with name " + targets[0].name + " has prop: " + targetKey);
+                        }
+
+                        console.log(targets[0].name + "'s hp is now " + targets[0].stats.hp + " and specifically the human's HP is " + this.characters["mole"].stats.hp);
+                    }
+                } else {
+                    // abl cost is an object map with keys that match the mutable resource stats... completely on purpose and by design, that was.
+                    for (costElement in chosenAbility.cost) {
+                        this.stats[costElement] -= chosenAbility.cost[costElement];
+                    }
+                }// end if no targets left after flag processing, so only abl cost is applied
+
+                console.log("The God's chosen abl is " + chosenAbility.name + " with first target named " + targets[0].name);
+            }// if role is enemy
+        }// if role is defined
+    }//end grue AI def
+    
     var theGodChar = this.characters["the_god"];
     theGodChar.gender = "male";
     theGodChar.stats["maxHP"] = 500;
@@ -313,7 +587,7 @@ function MootyWortRpgMech() {
     primordialMandateSpell.generateFlavorText = function (sourceChar, targetChar) {
         return "The God thrusts his tentacles into the earth and the ground begins to quake.  A deep rumble like the visceral warning growl of an intractable force of nature resonates with your spirit, awakening atavistic awe.  Auras of pulsing magma and wild verdancy course up and down The God's flesh in a violently unstoppable current, infusing every fiber of his being with all the might of this world!  The eyes of The God roll wildly, as if even the boundaries of his conception of all realities is being harshly tested by overwhelming new horizons."; 
     }
-    // todo: define dark_star (high MP cost for big damage)
+    // define dark_star (high MP cost for big damage)
     /**
     Dark Star deals massive non-elemental damage to all enemies
     */
@@ -431,16 +705,21 @@ function MootyWortRpgMech() {
                         }// end rando block
                     }// end The God HP > 25%
                     else {
-                        // being severely injured, The God now starts to spam Dark Star if no earlier behaviors were proced
-                        chosenAbility = this.spells["dark_star"];
-                        chosenTarget = playerParty;
+                        // being severely injured, The God now starts to spam Dark Star if no earlier behaviors were proced and he can afford it
+                    	if(theGodChar.canAffordCost(darkStarSpell)) {
+                    		chosenAbility = this.spells["dark_star"];
+                    		chosenTarget = playerParty;
+                    	} else {
+                    		chosenAbility = this.abilities["attack"];
+                    		chosenTarget = playerLeastDefense;
+                    	}
                     }
                 } // end if abl and target are not yet chosen, landing us in defaults
                 /// end defaults block ///
 
                 // normalize input chosen targets to array form
                 var targets = undefined;
-                if (chosenAbility.targetType === window.Ability.TargetTypesEnum.allEnemies) {
+                if (chosenAbility.targetType === lib.Ability.TargetTypesEnum.allEnemies) {
                     console.log("setting AI targets to array " + chosenTarget + " which starts with " + chosenTarget[0].name);
                     targets = chosenTarget;
                 } else {
