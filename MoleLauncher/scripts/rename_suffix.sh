25cdf6693e6a6febaa1bcbfe5fe5d96a820240d1
mysterymagination <creswel2@gmail.com>
2020-09-08 11:45:29 -0400

    rename script works for original suffix idea

--- MoleLauncher/scripts/rename_suffix.sh
+++ MoleLauncher/scripts/rename_suffix.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-idx=$1 
+targetDir=$1
+idx=$2 
+returnDir=$(pwd)
+echo "we'll be returning to "$returnDir
+cd $targetDir
+echo "we're now inside"$(pwd)
 for i in *.png 
 do 
-  echo $i
   baseStr=$(basename $i .png)
+  echo "adding _"$idx" to "$baseStr
   mv $i $baseStr"_"$idx".png" 
 done
+cd $returnDir
